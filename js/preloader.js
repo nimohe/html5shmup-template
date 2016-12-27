d260cd0fcbdad6bf2238776d5c8f1bcf5d20eacb
nimo <nimohe@protonmail.com>
2016-12-27 18:19:14 +0800

    center preloader ui

--- js/preloader.js
+++ js/preloader.js
@@ -15,8 +15,9 @@ BasicGame.Preloader.prototype = {
     //  Show the loading progress bar asset we loaded in boot.js
     this.stage.backgroundColor = '#2d2d2d';
 
-    this.preloadBar = this.add.sprite(412, 400, 'preloaderBar');
-    this.add.text(510, 360, "Loading...", { font: "32px monospace", fill: "#fff" }).anchor.setTo(0.5, 0.5);
+    this.preloadBar = this.add.sprite(this.game.width/2, this.game.height/2, 'preloaderBar');
+    this.preloadBar.x=(this.game.width-this.preloadBar.width)/2;
+    this.add.text(this.game.width/2, this.game.height/2+50, "Loading...", { font: "32px monospace", fill: "#fff" ,align:"center"}).anchor.setTo(0.5, 0.5);
 
     //  This sets the preloadBar sprite as a loader sprite.
     //  What that does is automatically crop the sprite from 0 to full-width
